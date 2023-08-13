a6e4f79deac3ae9284e8c656016f834c1c96f691
sru7 <142006776+sru7@users.noreply.github.com>
2023-08-13 09:47:07 +0800

    Update notion-hugo.config.ts

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -3,7 +3,7 @@ import { UserConfig } from "./src/config"
 const userConfig: UserConfig = {
     mount: {
         manual: false,
-        page_url: 'https://www.notion.so/Notion-DoIt-d25308322b54475aa2a04a603502e993',
+        page_url: 'https://inquisitive-clematis-99c.notion.site/Notion-DoIt-d25308322b54475aa2a04a603502e993',
         pages: [
             // {
             //     page_id: '<page_id>',
